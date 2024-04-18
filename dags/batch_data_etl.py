b761ccb3fcedc8790ec987f39a2750f6c24e1ff0
harry1321 <48749818+harry1321@users.noreply.github.com>
2024-04-18 12:04:42 +0800

    Update batch_data_etl.py

--- dags/batch_data_etl.py
+++ dags/batch_data_etl.py
@@ -2,9 +2,9 @@ from datetime import datetime,timedelta
 
 from airflow.models import DAG
 # Operators
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 
-from tasks.task_functions import task_date, task_gen, task_clean
+from tasks.task_functions import task_date, task_gen, task_clean, task_branch, task_upload
 
 default_args={
     "owner": 'Harry Yang',
@@ -31,4 +31,21 @@ with DAG(
         python_callable=task_clean
     )
 
-date >> gen >> clean
+    branch = BranchPythonOperator(
+        task_id="branch",
+        python_callable=task_branch,
+        op_kwargs={"success_route":"upload_processed", "failed_route":"upload_unprocessed"}
+    )
+
+    upload_processed = PythonOperator(
+        task_id="upload_processed",
+        python_callable=task_upload,
+        op_kwargs={"data_state":"processed"}
+    )
+    
+    upload_unprocessed = PythonOperator(
+        task_id='upload_unprocessed',
+        python_callable=task_upload,
+        op_kwargs={"data_state":"unprocessed"}
+    )
+date >> gen >> clean >> branch >> [upload_processed, upload_unprocessed]
